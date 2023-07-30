e088102a69851dd1f4bec1363f22924de9c7a872
adhiiimf <adhimfahmi@gmail.com>
2023-07-30 20:15:48 +0700

    update ui

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
 export default {
   data() {
     return {
+      profileName: '',
       linkedinlink: '',
+      wanumber: '',
+      ig: '',
+      fb: '',
+      yt: '',
+      github: '',
+      email: '',
       show: true
     }
   }
@@ -10,56 +17,52 @@ export default {
 </script>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import GreetingHello from './components/GreetingHello.vue';
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <GreetingHello visitor="Adhi"/>
-    </div>
-  </header>
-
-  <main>
-    <input v-model="linkedinlink" type="text">
-    <h1>{{ linkedinlink }}</h1>
-    <button @click="show = !show">Toggle</button>
-    <Transition name="bounce">
-      <p v-if="show" style="text-align: center;">
-        Hello here is some bouncy text!
-      </p>
-    </Transition>
-  </main>
+  <section>
+    <header>
+      <div class="wrapper">
+        <GreetingHello :visitor="profileName"/>
+      </div>
+    </header>
+    <main class="w-1/2 pr-12 mt-10">
+      <div class="bg-gray-100  shadow-inner rounded-xl font-semibold items-center flex">
+        <font-awesome-icon icon="fas fa-user-astronaut" class="ml-4 text-gray-600" />
+        <input v-model="profileName" class="w-full bg-transparent rounded-xl py-2 mx-3 outline-none" type="text" placeholder="Adhi ..." required>
+      </div>
+      <div class="bg-gray-100 shadow-inner rounded-xl font-semibold items-center flex mt-4">
+        <font-awesome-icon icon="fa-solid fa-envelope" class="ml-4 text-gray-600" />
+        <input v-model="email" class="w-full bg-transparent rounded-xl py-2 mx-3 outline-none" type="text" placeholder="email@gmail.com" required>
+      </div>
+      <button class="w-full py-2 mt-4 bg-gradient-to-tr from-emerald-500 to-green-300 shadow-inner rounded-xl font-semibold items-center flex justify-center text-white text-xl">
+        +
+      </button>
+    </main>
+    <aside class="md:fixed flex top-0 right-0 z-40 md:w-1/2 w-full max-md:rounded-2xl h-screen transition-transform translate-x-full sm:translate-x-0 bg-gradient-to-t from-cyan-300 to-green-200 shadow-inner justify-center pt-8">
+        <div class="p-5 flex justify-center">
+          <ul class=" flex flex-col items-center">
+            <li v-show="profileName.length > 0" class="flex justify-center items-center mb-2">
+              <font-awesome-icon icon="fas fa-user-astronaut" class="text-gray-600 text-5xl" />
+            </li>
+            <li v-show="profileName.length > 0" class="flex item-center mb-2">
+              <h3 class="font-semibold text-center text-3xl"> <span class="text-2xl font-semibold">Hi!</span> <br> I'm {{ profileName }}</h3>
+            </li>
+            <li v-show="email.length > 0" class="flex item-center my-2">
+              <a :href="'mailto:' + email" class="flex items-center justify-center w-96 h-12 rounded-xl bg-gradient-to-tr from-green-500 to-emerald-500 ">
+                <font-awesome-icon icon="fa-solid fa-envelope" class="text-gray-100 text-xl mr-2" />
+                <h3 class="font-semibold text-gray-100">{{ email }}</h3>
+              </a>
+            </li>
+            <li v-show="email.length > 0" class="flex item-center my-2">
+              <a :href="'mailto:' + email" class="flex items-center justify-center w-96 h-12 rounded-xl bg-gradient-to-tr from-green-500 to-emerald-500 ">
+                <font-awesome-icon icon="fa-solid fa-envelope" class="text-gray-100 text-xl mr-2" />
+                <h3 class="font-semibold text-gray-100">{{ email }}</h3>
+              </a>
+            </li>
+          </ul>
+        </div>
+    </aside>
+  </section>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
