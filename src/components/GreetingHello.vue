ce522bc2a22d95ffaea494c42c8b01922172c999
adhiiimf <adhimfahmi@gmail.com>
2023-07-30 20:16:58 +0700

    create Greeting Hello DOM

--- src/components/GreetingHello.vue
+++ src/components/GreetingHello.vue
@@ -8,9 +8,12 @@ defineProps({
 </script>
 
 <template>
-    <div>
-        <h1>Hi, {{ visitor }}</h1>
-        <h2>Welcome to Simplyourlink</h2>
-        <span>Create your contact link simply with me</span>
+    <div class="flex items-center">
+        <font-awesome-icon icon="hands-clapping" class="text-8xl mr-4" />
+        <div>
+            <h1 class="text-4xl italic">Hi<span :class="{}">,</span> <span class=" font-bold">{{ visitor }}</span></h1>
+            <h2 class="text-xl my-2">Welcome to <span class=" text-emerald-500 font-bold">simplyour</span><span class="text-green-600 font-bold">link</span></h2>
+            <span class="text-semibold">make your link with us</span>
+        </div>
     </div>
 </template>
